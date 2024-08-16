907811888037d444e696db86b2cfe87d81532ff3
Larsianer <larsianer3141@gmail.com>
2024-08-16 13:01:51 +0200

    update flake to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "Driver for LEDs in my room";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
     outputs = { self, nixpkgs, ...}: let
--- include/index.h
+++ include/index.h
@@ -25,10 +25,16 @@ label {
     margin: 10px;
     border-width: 2px;
     border-radius: 4px;
-    border-color: white;
+    border-color: #e7e7ed;
     border-style: solid;
     box-sizing: border-box;
     width: min-content;
+}
+
+.grid-container {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 20% 20%;
 }
         </style>
         <script>
@@ -51,20 +57,30 @@ label {
     <!-- execute previewColor once on page load to directly show the correct color --!>
     <body onload="previewColor()">
         <form method="post">
-            <div class="container">
-                <input type="checkbox" name="enable" id="enable"/>
-                <label for="enable">Enable LEDs</label>
-                <input type="submit" id="update" value="Update LEDs">
-            </div>
-            <div class="container" id="color-preview">
-            </div>
-            <div class="container" id="color-picker">
-                <input type="range" min=0 max=255 name="hue" id="hue" class="color-picker-range" oninput="previewColor()"/>
-                <label for="hue">Hue</label>
-                <input type="range" min=0 max=255 name="sat" id="sat" class="color-picker-range" oninput="previewColor()"/>
-                <label for="sat">Saturation</label>
-                <input type="range" min=0 max=255 name="val" id="val" class="color-picker-range" oninput="previewColor()"/>
-                <label for="val">Value</label>
+            <div class="grid-container">
+                <div class="container">
+                    <input type="checkbox" name="enable" id="enable"/>
+                    <label for="enable">Enable LEDs</label>
+                    <input type="submit" id="update" value="Update LEDs">
+                </div>
+                <div class="container" id="color-preview">
+                </div>
+                <div class="container" id="color-picker">
+                    <input type="range" min=0 max=255 name="hue" id="hue" class="color-picker-range" oninput="previewColor()"/>
+                    <label for="hue">Hue</label>
+                    <input type="range" min=0 max=255 name="sat" id="sat" class="color-picker-range" oninput="previewColor()"/>
+                    <label for="sat">Saturation</label>
+                    <input type="range" min=0 max=255 name="val" id="val" class="color-picker-range" oninput="previewColor()"/>
+                    <label for="val">Value</label>
+                </div>
+                <div class="container">
+                </div>
+                <div class="container">
+                </div>
+                <div class="container">
+                </div>
+                <div class="container">
+                </div>
             </div>
         </form>
     </body>
